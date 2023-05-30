c9d58ebbc7ecca14f1f8cb65229968cd1eaf238d
Lukas Hammerschmid <7109@htl.rennweg.at>
2023-05-30 13:22:00 +0200

    Fixed WeeklyProgram issue in the Webapp

--- KinoProgram.Webapp/Pages/Cinema/Index.cshtml.cs
+++ KinoProgram.Webapp/Pages/Cinema/Index.cshtml.cs
@@ -10,19 +10,34 @@ namespace KinoProgram.Webapp.Pages.Cinema
 {
     public class IndexModel : PageModel
     {
+        public record WeeklyProgramDto(
+            int WeekNumber,
+            string GenreList,
+            int MoviesCount);
         private readonly CinemaContext _db;
-        public List<WeeklyProgram> WeeklyProgams { get; private set; } = new();
+        public List<WeeklyProgramDto> WeeklyProgams { get; private set; } = new();
         public IndexModel(CinemaContext db)
         {
             _db = db;
         }
         public void OnGet()
         {
-            WeeklyProgams = _db.WeeklyPrograms.GroupBy(g => g.CalendarWeek).Select(s => new WeeklyProgram
-            {
-                CalendarWeek = s.Key,
-                Movies = s.Count(),
-            }).ToList();
+            var weeklyPrograms = _db.WeeklyPrograms
+                .OrderBy(w=>w.CalendarWeek)
+                .GroupBy(g => g.CalendarWeek)
+                .Select(g => new WeeklyProgramDto(
+                    g.Key,
+                    string.Join(", ", g.Select(g => g.Movie.MovieCategory.ToString())),
+                    g.Count()))
+                .ToList();
+            WeeklyProgams = Enumerable.Range(1, 52)
+                .GroupJoin(weeklyPrograms, w => w, w => w.WeekNumber, (week, movies) =>
+                {
+                    if (movies.Any()) { return movies.First(); }
+                    return new WeeklyProgramDto(week, string.Empty, 0);
+                })
+                .OrderBy(m => m.WeekNumber)
+                .ToList();
         }
     }
 }
